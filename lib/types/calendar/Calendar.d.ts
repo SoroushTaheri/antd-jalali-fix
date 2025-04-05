1c179f367dcc43d577d1a8c4562ee1130413ca0d
Soroush Taheri <soroushtgh@gmail.com>
2025-04-05 14:19:40 +0330

    Add lib build files

---
+++ lib/types/calendar/Calendar.d.ts
@@ -0,0 +1,4 @@
+import { Dayjs } from "dayjs";
+declare const Calendar: import("react").FC<Readonly<import("antd/es/calendar/generateCalendar").CalendarProps<Dayjs>>>;
+export default Calendar;
+//# sourceMappingURL=Calendar.d.ts.map
